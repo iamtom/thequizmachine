cdb8434b77a5b3abd3a94cb87df78eb58a64e551
Tom D <tomd.470@gmail.com>
2023-05-31 21:57:30 +0100

    Added deleteQuestion method to QuestionController

--- src/main/java/tom/iamtom/thequizmachine/controllers/QuestionController.java
+++ src/main/java/tom/iamtom/thequizmachine/controllers/QuestionController.java
@@ -11,6 +11,7 @@ import org.springframework.hateoas.IanaLinkRelations;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -80,5 +81,13 @@ public class QuestionController {
         return ResponseEntity
                 .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
                 .body(entityModel);
+    }
+    
+    @DeleteMapping("/questions/{id}")
+    public ResponseEntity<?> deleteQuestion(@PathVariable Long id) {
+        questionRepository.deleteById(id);
+        
+        //This returns an HTTP 204 No Content response.
+        return ResponseEntity.noContent().build();
     }
 }
